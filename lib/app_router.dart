bc13255666fb4e9aff3934c39c22b5bc1c63cadb
kvartalovea <kvartalovefade@gmail.com>
2025-10-30 22:17:13 +0300

    Реализация маршрутной карты

---
+++ lib/app_router.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'main_screen/main.dart';
+import 'number_screen/number_screen.dart';
+import 'points_screen/points_screen.dart';
+import 'assist_screen/assists_screen.dart';
+import 'injury_screen/injury_screen.dart';
+
+class AppRouter {
+  static const String profileRoute = '/';
+  static const String numberRoute = '/number';
+  static const String pointsRoute = '/points';
+  static const String assistsRoute = '/assists';
+  static const String injuryRoute = '/injury';
+
+  static final GoRouter router = GoRouter(
+    initialLocation: profileRoute,
+    routes: [
+      GoRoute(
+        path: profileRoute,
+        name: 'profile',
+        builder: (context, state) => const PlayerProfileScreen(),
+      ),
+      GoRoute(
+        path: numberRoute,
+        name: 'number',
+        builder: (context, state) => const NumberScreen(),
+      ),
+      GoRoute(
+        path: pointsRoute,
+        name: 'points',
+        builder: (context, state) => const PointsScreen(),
+      ),
+      GoRoute(
+        path: assistsRoute,
+        name: 'assists',
+        builder: (context, state) => const AssistsScreen(),
+      ),
+      GoRoute(
+        path: injuryRoute,
+        name: 'injury',
+        builder: (context, state) => const InjuryScreen(),
+      ),
+    ],
+  );
+}
